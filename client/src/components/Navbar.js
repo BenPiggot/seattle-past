54fcc51f4a03444aa6babd43ec4d21105b740021
ben <wbpiggot@gmail.com>
2017-09-05 22:43:56 -0700

    Hooking up some login info

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,17 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import AppBar from 'material-ui/AppBar';
-import {grey300} from 'material-ui/styles/colors.js'
+import { Tab } from 'material-ui/Tabs';
+import {grey300} from 'material-ui/styles/colors.js';
+import * as actions from '../actions';
 
 /**
  * A simple example of `AppBar` with an icon on the right.
  * By default, the left icon is a navigation-menu.
  */
-const Navbar = () => (
-  <AppBar
-    style={{backgroundColor: grey300, color: '#999', fontFamily: 'Raleway'}}
-    title="Seattle Past"
-    iconClassNameRight="muidocs-icon-navigation-expand-more"
-  />
-);
+class Navbar extends Component {
+  constructor(props) {
+    super(props);
 
-export default Navbar;
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  componentDidMount() {
+    this.props.fetchUser()
+  }
+
+  handleClick() {
+    console.log('clicked')
+  }
+
+  render() {
+    return (
+      <AppBar
+        style={{backgroundColor: grey300, color: '#999', fontFamily: 'Raleway'}}
+        title="Seattle Past"
+        iconClassNameRight="muidocs-icon-navigation-expand-more"
+      >
+        { this.props.auth ? <Tab onClick={this.handleClick} label={`Welcome ${this.props.auth.username}`} /> : null }
+      </AppBar>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    auth: state.auth
+  }
+}
+
+export default connect(mapStateToProps, actions)(Navbar);
--- client/src/index.js
+++ client/src/index.js
@@ -4,13 +4,15 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
 import logger from 'redux-logger';
+
+import reducers from './reducers';
 import App from './App';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import './App.css';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
 
-const store = createStore(() => {}, {}, applyMiddleware(logger, reduxThunk));
+const store = createStore(reducers, {}, applyMiddleware(logger, reduxThunk));
 
 render(<Provider store={store}>
         <MuiThemeProvider>
