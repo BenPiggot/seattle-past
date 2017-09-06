9f473dd81e238c96c5a5074f16f51bd7c0443333
ben <wbpiggot@gmail.com>
2017-09-06 14:54:59 -0700

    A few more updates

--- models/User.js
+++ models/User.js
@@ -4,6 +4,7 @@ const { Schema } = mongoose;
 const userSchema = Schema({
   facebookId: String,
   twitterId: String,
+  instagramId: String,
   email: String,
   username: String,
   avatar: String,
