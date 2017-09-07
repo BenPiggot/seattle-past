d3b9737064b2f64432f604762cf9264a11a5adbc
ben <wbpiggot@gmail.com>
2017-09-06 19:48:57 -0700

    A bit of cleanup

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,22 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 import SeattleMap from './Map';
 import './Landing.css';
 
-class Landing extends Component {
-  render() {
-    return (
-      <div>
-        <SeattleMap zoom={12}/>
-        <div className="overlay" />
-        <div className="sign-in">
-          <h1 className="title">Ghosts of Seattle Past</h1>
-          <h1><a href='/auth/facebook'>Sign in with Facebook</a></h1>
-          <h1><a href='/auth/twitter'>Sign in with Twitter</a></h1>
-          <h1><a href='/auth/instagram'>Sign in with instagram</a></h1>
-        </div>
+const Landing = () => {
+  return (
+    <div>
+      <SeattleMap zoom={12}/>
+      <div className="overlay" />
+      <div className="sign-in">
+        <h1 className="title">Ghosts of Seattle Past</h1>
+        <h1><a href='/auth/facebook'>Sign in with Facebook</a></h1>
+        <h1><a href='/auth/twitter'>Sign in with Twitter</a></h1>
+        <h1><a href='/auth/instagram'>Sign in with instagram</a></h1>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default Landing;
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import AppBar from 'material-ui/AppBar';
 import Drawer from 'material-ui/Drawer';
@@ -14,13 +15,16 @@ import * as actions from '../actions';
 class Navbar extends Component {
   constructor(props) {
     super(props);
-    this.state = {open: false};
+    this.state = {open: false}; 
 
+    this.handleRoutes = this.handleRoutes.bind(this);
     this.handleToggle = this.handleToggle.bind(this);
-
     this.handleClose = this.handleClose.bind(this);
-
     this.handleClick = this.handleClick.bind(this);
+  }
+
+  static contextTypes = {
+    router: PropTypes.object
   }
 
   componentDidMount() {
@@ -39,22 +43,34 @@ class Navbar extends Component {
     this.setState({ open: false })
   }
 
+  handleRoutes(e, route) {
+    this.context.router.history.push(route)
+    this.handleClose()
+  }
+
   render() {
     return (
       <AppBar
         style={{backgroundColor: grey300, color: '#999', fontFamily: 'Raleway'}}
         title="Seattle Past"
+        onLeftIconButtonTouchTap={this.handleToggle}
         iconClassNameRight="muidocs-icon-navigation-expand-more"
       >
         { this.props.auth ? <Tab onClick={this.handleClick} label={`Welcome ${this.props.auth.username}`} /> : null }
         <Drawer
           docked={false}
-          width={200}
+          width={250}
           open={this.state.open}
           onRequestChange={(open) => this.setState({open})}
         >
-          <MenuItem onClick={this.handleClose}>Menu Item</MenuItem>
-          <MenuItem onClick={this.handleClose}>Menu Item 2</MenuItem>
+          <MenuItem onClick={(e) => this.handleRoutes(e, '/')}>Home</MenuItem>
+          <MenuItem onClick={(e) => this.handleRoutes(e, '/capitol-hill')}>Capitol Hill and the Central Area</MenuItem>
+          <MenuItem onClick={(e) => this.handleRoutes(e, '/rainier-valley')}>Rainier Valley</MenuItem>
+          <MenuItem onClick={(e) => this.handleRoutes(e, '/west-seattle')}>West Seattle</MenuItem>
+          <MenuItem onClick={(e) => this.handleRoutes(e, '/north-seattle')}>North Seattle</MenuItem>
+          <MenuItem onClick={(e) => this.handleRoutes(e, '/ballard')}>Ballard</MenuItem>
+          <MenuItem onClick={(e) => this.handleRoutes(e, '/udistrict')}>The U District and Fremont</MenuItem>
+          <MenuItem onClick={(e) => this.handleRoutes(e, '/downtown')}>Downtown and Queen Anne</MenuItem>
         </Drawer>
       </AppBar>
     );
